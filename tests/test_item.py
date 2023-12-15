c4a394351276490c965b7db0a063e64adab774b5
yoshagg <smertinkirill@gmail.com>
2023-12-15 22:54:59 +0300

    stevehuys added

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture()
+def stevehuys():
+    return Item("stevehuys", 228, 1)
+
+
+def test_item_calculate_total_price(stevehuys):
+    assert stevehuys.calculate_total_price() == 228
+
+
+def test_apply_discount(stevehuys):
+    Item.pay_rate = 0.8
+    stevehuys.apply_discount()
+    assert stevehuys.price == 182.4
