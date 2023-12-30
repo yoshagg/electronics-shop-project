e1917c26e7cd55121427963902da6529394986bb
yoshagg <smertinkirill@gmail.com>
2023-12-30 20:32:06 +0300

    problem hmwrk2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+import pd
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -30,3 +33,30 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * Item.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) > 10:
+            self.__name = new_name[0:9]
+        else:
+            self.__name = new_name
+        return 0
+
+    def instantiate_from_csv(self, csv_file_name):
+        data = pd.read_csv(csv_file_name, skiprows=1, sep=';', index_col=False,
+                           names=["CSV_Item1", "CSV_Item2", "CSV_Item3", "CSV_Item4", 'CSV_Item5'])
+        for _, row in data.iterrows():
+            self.csvitem1 = row['CSV_Item1']
+            self.csvitem2 = row['CSV_Item2']
+            self.csvitem3 = row['CSV_Item3']
+            self.csvitem4 = row['CSV_Item4']
+            self.csvitem5 = row['CSV_Item5']
+        return 0
+
+    def string_to_number(self, param):
+        return int(param)
+
